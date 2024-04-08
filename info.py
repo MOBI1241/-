d6cd67e59a4876e79021860d9464ca6f0ea85e2f
MOBI1241 <158516807+MOBI1241@users.noreply.github.com>
2024-04-08 19:12:40 +0300

    Update info.py

--- info.py
+++ info.py
@@ -87,8 +87,8 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Shortlink Info
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'hRPS5vvZc0OGOEUQJMJzPiojoVK2')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.ouo.io')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'VfkblbaW')
 
 # Others
 MAX_B_TN = environ.get("MAX_B_TN", "5")
